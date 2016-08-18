9b9dd94215e6ebfddb26872b5d11dbc6b504ae65
stasinua <stas.in.ua@gmail.com>
2016-08-18 21:06:18 +0300

    Added meiligao protocol support.

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 4000;
-var app = express();
-app.use(bodyParser.json());
+// var express = require('express');
+// var path = require('path');
+// var bodyParser = require('body-parser');
+// var port = process.env.PORT || 4000;
+// var app = express();
+// app.use(bodyParser.json());
+//
+// app.get('*', function(req,res){
+//   var request = {
+//     query: req.query,
+//     url: req.url
+//   };
+//   console.log("request",request);
+//   console.log(req);
+//   res.json({request: request});
+// });
+//
+// app.listen(port);
+// console.log("busTracker on " + port);
+//
+// module.exports = app;
+var meiligao = require('meiligao');
 
-app.get('*', function(req,res){
-  var request = {
-    query: req.query,
-    url: req.url
-  };
-  console.log("request",request);
-  console.log(req);
-  res.json({request: request});
+var server = new meiligao.Server({
+    timeout: 120000
+}).listen(4000, function(error) {
+    if (error) throw error;
+    console.log('gps server is listening');
 });
 
-app.listen(port);
-console.log("busTracker on " + port);
-
-module.exports = app;
+server.on('connect', function(tracker) {
+    console.log('tracker connected!');
+});
