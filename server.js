018b8cef859a7ded28d74d8820d38ea5d82cfcb5
stasinua <stas.in.ua@gmail.com>
2016-08-22 21:25:59 +0300

    Set port to 5009 and timing to 60 seconds.

--- server.js
+++ server.js
@@ -21,13 +21,56 @@
 // module.exports = app;
 var meiligao = require('meiligao');
 
-var server = new meiligao.Server({
-    timeout: 6000
-}).listen(4000, function(error) {
-    if (error) throw error;
-    console.log('gps server is listening');
+// Set up server
+var server = new meiligao.Server({timeout: 60000}).listen(5009, function(error) {
+  if (error) {
+    throw error;
+  }
+  console.log('gps server is listening');
 });
 
+// Handle server connections
 server.on('connect', function(tracker) {
-    console.log('tracker connected!');
+  console.log('tracker has connected');
+
+  /**
+   * Tracker events
+   */
+  // Tracker requested login (and it was confirmed)
+  tracker.on('login', function(tracker) {
+    console.log('tracker has logged in');
+  });
+
+  // Heartbeat packets
+  tracker.on('heartbeat', function(tracker) {
+    console.log('tracker sent heartbeat');
+  });
+
+  // Most useful thing: alarms & reports
+  tracker.on('message', function(message, tracker) {
+    console.log('tracker sent message: [' + meiligao.Message.getMessageTypeByCode(message.type) + ']', message);
+  });
+
+  // Useful for debugging
+  tracker.on('packet.in', function(packet, tracker) {
+    console.log('incoming packet:', packet.toString());
+  });
+  tracker.on('packet.out', function(packet, tracker) {
+    console.log('outgoing packet:', packet.toString());
+  });
+
+  // Handle errors
+  tracker.on('error', function(error, buffer) {
+    console.log('error parsing message:', error, buffer);
+  });
+
+  // Handle disconnects
+  tracker.on('disconnect', function() {
+    console.log('tracker disconnected (tracker.disconnect)');
+  });
+});
+
+// Handle disconnects
+server.on('disconnect', function(tracker) {
+  console.log('tracker disconnected (server.disconnect)');
 });
